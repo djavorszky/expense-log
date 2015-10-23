ae1098797e5c833bdc33be85f52e922bb45359b6
djavorszky <daniel.javorszky@gmail.com>
2015-10-23 16:55:15 +0200

    Add example usage to expense-log.sh

--- expense-log.sh
+++ expense-log.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-#java ./out/production/expense-log/com/expenselog/Main.class 
-java -cp ./out/production/expense-log com.expenselog.Main "$@"
+if [[ $# -eq 0 ]]; then
+	echo "Example usage:"
+	echo "./expense-log.sh -c add-expense -a [amount] -ct category [-d date]"
+	echo "./exepense-log.sh -c list"
+else 
+	java -cp ./bin/production/expense-log com.expenselog.Main "$@"
+fi
